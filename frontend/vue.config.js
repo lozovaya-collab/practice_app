12e1f7ddcbc944d62fe56dd943913fc201d73bb7
lozovaya-collab <anyalozovaya2014@gmail.com>
2023-04-12 23:28:41 +0300

    fix: dockerfiles

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production' ?
-        '/drag_grop_vue/' : '/',
+        './' : '/',
 
 
     pages: {
@@ -18,4 +18,4 @@ module.exports = {
 
     }
 
-}
+};
